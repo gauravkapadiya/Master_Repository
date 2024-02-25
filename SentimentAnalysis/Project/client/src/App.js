29d886da513dc2d9e0df2354f8bd05d1a34b94b0
Gaurav Kapadiya <65489691+gauravkapadiya@users.noreply.github.com>
2024-02-25 19:23:29 +0100

    Add files via upload

---
+++ SentimentAnalysis/Project/client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import ReviewRating from './components/ReviewRating';
+
+function App() {
+  return (
+    <div>
+      <ReviewRating />
+    </div>
+  );
+}
+
+export default App;
